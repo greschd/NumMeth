c6b4fa711e320a32960fd9272337cb957aee4156
Dominik Gresch <greschd@ethz.ch>
2014-02-14 22:27:23 +0100

    arrayindex bsp hinzugefügt

---
+++ examples/general/arrayindex.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3.2
+# -*- coding: utf-8 -*-
+#
+# Author:  Dominik Gresch <greschd@ethz.ch>
+# Date:    14.02.2014 21:55:16 CET
+# File:    arrayindex.py
+
+import sys
+import numpy as np
+
+if __name__ == "__main__":
+    
+    A = np.array([1, 2, 3, 4])
+    
+    print(A[0]) # Indizierung beginnt bei 0
+    print(A[1])
+    print(A[-1]) # Minus - erstes Element == letztes Element
+    print(A[-2])
+    
+    print('\n')
+    
+    size = 4;
+    line = lambda n, m: [x for x in xrange((n * m),((n + 1) * m))]
+    B = np.array([line(n, size) for n in xrange(0, size)])
+    print B
+    
+    print('\n')
+    
+    # Variante 1: einzelne Elemente
+    print B[(0,1),(1,2)]
+    
+    print('\n')
+    
+    # Variante 2: alle Kombinationen der Zeilen - und Spaltenindizes
+    i = np.array([[0],[1]])
+    j = np.array([[1,2]])
+    print B[i,j]
+    
+    print('\n')
+    
+    # Achte auf die doppelte Klammerung (die doppelte Klammer wird zur Zeile, die einfache zur Spalte)
+    i = np.array([[0,1]])
+    j = np.array([[1],[2]])
+    print B[i,j]
