d1871938edd5197bd0063ccada494db1c7ecd3c8
Dominik Gresch <greschd@ethz.ch>
2014-02-25 23:18:36 +0100

    added titles to arrayindex.py

--- examples/general/arrayindex.py
+++ examples/general/arrayindex.py
@@ -12,6 +12,8 @@ if __name__ == "__main__":
     
     A = np.array([1, 2, 3, 4])
     
+    
+    print("Bsp 1")
     print(A[0]) # Indizierung beginnt bei 0
     print(A[1])
     print(A[-1]) # Minus - erstes Element == letztes Element
@@ -19,6 +21,7 @@ if __name__ == "__main__":
     
     print('\n')
     
+    print("Bsp 2")
     size = 4;
     line = lambda n, m: [x for x in range((n * m),((n + 1) * m))]
     B = np.array([line(n, size) for n in range(0, size)])
@@ -26,12 +29,15 @@ if __name__ == "__main__":
     
     print('\n')
     
+    print("Bsp 3")
     # Variante 1: einzelne Elemente
+    print("Variante 1")
     print B[(0,1),(1,2)]
     
     print('\n')
     
     # Variante 2: alle Kombinationen der Zeilen - und Spaltenindizes
+    print("Variante 2")
     i = np.array([[0],[1]])
     j = np.array([[1,2]])
     print B[i,j]
